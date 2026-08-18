aef75bb1398555ff15d2c4f9ecc9e8d88918fba8
Humming-Moon-afk <milian.loechner@gmail.com>
2026-08-18 13:44:15 +0200

    feat(client): add header and footer sections to welcome screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,9 +3,15 @@ import { Text, View } from 'react-native'
 
 export default function() {
   return(
-    <View style ={{justifyContent: 'center', flex: 1, alignItems:'center', gap: 5}}>
-      <Text style = {{fontSize: 50}}>Hinge</Text>
-      <Text style = {{fontSize: 18}}>Designed to be deleted</Text>
+    <View style = {{flex: 1, justifyContent: 'space-between'}}>
+      <View style ={{paddingTop: 180, alignItems: 'center', gap: 20}}>
+        <Text style = {{fontSize: 50}}>Hinge</Text>
+        <Text style = {{fontSize: 18}}>Designed to be deleted</Text>
+      </View>
+      <View style = {{paddingBottom: 240, alignItems: 'center', gap: 30}}>
+        <Text style = {{fontSize: 14}}>Konto erstellen</Text>
+        <Text style = {{fontSize: 14}}>Anmelden</Text>
+      </View>
     </View>
   )
 }
@@ -16,8 +22,10 @@ export default function() {
 
 
 // flexDirection: Richtung der Hauptachse ('column' = vertikal, 'row' = horizontal)
+
 // justifyContent: Ausrichtung auf der Hauptachse ('center', 'space-between', ...)
 // alignItems: Ausrichtung auf der Querachse ('center', 'stretch', ...)
+
 // gap: Fester Abstand zwischen Kind-Elementen
 // padding: Innenabstand (Platz innerhalb der Box)
 // margin: Außenabstand (Abstand zu Nachbar-Boxen)
