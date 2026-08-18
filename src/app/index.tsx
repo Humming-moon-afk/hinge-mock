5809d4557bf3e8d13a3ffa7353425011897e8c7c
Humming-Moon-afk <milian.loechner@gmail.com>
2026-08-18 13:08:42 +0200

    feat(client): add initial welcome screen layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,8 +3,9 @@ import { Text, View } from 'react-native'
 
 export default function() {
   return(
-    <View style={{flex: 1, paddingTop: 60, paddingHorizontal: 20, backgroundColor: '#ffffff'}}>
-      <Text>Bitte Gib deine Telefonnummer ein: </Text>
+    <View style ={{justifyContent: 'center', flex: 1, alignItems:'center', gap: 5}}>
+      <Text style = {{fontSize: 50}}>Hinge</Text>
+      <Text style = {{fontSize: 18}}>Designed to be deleted</Text>
     </View>
   )
 }
