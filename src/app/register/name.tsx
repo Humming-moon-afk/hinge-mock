099286328e8367ac154cab1c34aa72bb58b773fe
Humming-Moon-afk <milian.loechner@gmail.com>
2026-08-20 12:41:57 +0200

    feat(register): add text input for user name

--- src/app/register/name.tsx
+++ src/app/register/name.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from 'react-native'
+import { Text, TextInput, View } from 'react-native'
 
 export default function() {
     return(
     <View style={{flex: 1, justifyContent:'center', alignItems:'center', paddingBottom: 100, gap: 30}}>
         <Text style = {{fontSize: 15}}>Wie lautet dein Name?</Text>
-        <Text style = {{fontSize: 12, borderRadius: 30, backgroundColor:'lightgrey', width: '35%', paddingVertical: 10, paddingHorizontal:10}}>Bitte eingeben...</Text>
+        <TextInput
+            style = {{fontSize: 12, borderRadius: 30, backgroundColor:'lightgrey', width: '35%', paddingVertical: 10, paddingHorizontal:10}}
+            placeholder='Bitte eingeben...'
+        />
     </View>
+    
     )
 }
