173e9936b7065d6d192b0d91cbff057381d26cb4
Humming-Moon-afk <milian.loechner@gmail.com>
2026-08-20 12:47:26 +0200

    feat(register): bind name input to local component state

--- src/app/register/name.tsx
+++ src/app/register/name.tsx
@@ -1,10 +1,12 @@
+import { useState } from 'react'
 import { Text, TextInput, View } from 'react-native'
 
 export default function() {
+    const [name, setName] = useState('')
     return(
     <View style={{flex: 1, justifyContent:'center', alignItems:'center', paddingBottom: 100, gap: 30}}>
         <Text style = {{fontSize: 15}}>Wie lautet dein Name?</Text>
-        <TextInput
+        <TextInput value={name} onChangeText={setName}
             style = {{fontSize: 12, borderRadius: 30, backgroundColor:'lightgrey', width: '35%', paddingVertical: 10, paddingHorizontal:10}}
             placeholder='Bitte eingeben...'
         />
