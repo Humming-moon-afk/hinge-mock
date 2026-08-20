15a452c2f12ca39d30bf60f43bcbe3c97b9e81a2
Humming-Moon-afk <milian.loechner@gmail.com>
2026-08-20 12:37:41 +0200

    feat(register): create initial name screen layout

--- src/app/register/name.tsx
+++ src/app/register/name.tsx
@@ -2,8 +2,9 @@ import { Text, View } from 'react-native'
 
 export default function() {
     return(
-    <View style={{flex: 1, justifyContent:'center'}}>
-        <Text>Wie lautet dein Name?</Text>
+    <View style={{flex: 1, justifyContent:'center', alignItems:'center', paddingBottom: 100, gap: 30}}>
+        <Text style = {{fontSize: 15}}>Wie lautet dein Name?</Text>
+        <Text style = {{fontSize: 12, borderRadius: 30, backgroundColor:'lightgrey', width: '35%', paddingVertical: 10, paddingHorizontal:10}}>Bitte eingeben...</Text>
     </View>
     )
 }
