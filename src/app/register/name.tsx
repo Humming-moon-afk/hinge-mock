87bfe3c16d16521e2626eaff683884408b03f8c4
Humming-Moon-afk <milian.loechner@gmail.com>
2026-08-20 12:28:50 +0200

    fix(navigation): replace tabs with stack layout to enable screen routing

---
+++ src/app/register/name.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function() {
+    return(
+    <View style={{flex: 1, justifyContent:'center'}}>
+        <Text>Wie lautet dein Name?</Text>
+    </View>
+    )
+}
