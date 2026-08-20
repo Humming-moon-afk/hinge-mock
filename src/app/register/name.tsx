c2dd863429459ff2185e7eb456e8886777454cc0
Humming-Moon-afk <milian.loechner@gmail.com>
2026-08-20 15:47:48 +0200

    feat(register): add continue button and polish name screen UI

--- src/app/register/name.tsx
+++ src/app/register/name.tsx
@@ -4,14 +4,14 @@ import { Text, TextInput, TouchableOpacity, View } from 'react-native'
 export default function() {
     const [name, setName] = useState('')
     return(
-    <View style={{flex: 1, justifyContent:'center', alignItems:'center', paddingBottom: 100, gap: 30}}>
-        <Text style = {{fontSize: 15}}>Wie lautet dein Name?</Text>
+    <View style={{paddingTop: 300, alignItems: 'center', gap: 20}}>
+        <Text style = {{fontSize: 18, fontWeight: 'bold'}}>Wie lautet dein Name?</Text>
         <TextInput value={name} onChangeText={setName}
-            style = {{fontSize: 12, borderRadius: 30, backgroundColor:'lightgrey', width: '35%', paddingVertical: 10, paddingHorizontal:10}}
+            style = {{fontSize: 12,fontWeight:'bold', borderRadius: 30, backgroundColor:'lightgrey', width: '35%', paddingVertical: 10, paddingHorizontal:10}}
             placeholder='Bitte eingeben...'
         />
-    <TouchableOpacity style = {{borderRadius:20, backgroundColor:'pink', paddingVertical:15, paddingHorizontal:10}}>
-        <Text>Weiter</Text>
+    <TouchableOpacity style = {{paddingVertical: 15, paddingHorizontal:10, borderRadius:30, alignItems:'center', backgroundColor: '#A689E1', width: '40%'}}>
+        <Text style = {{alignContent: 'center', fontSize: 18}}>Weiter</Text>
     </TouchableOpacity>
     </View>
     
