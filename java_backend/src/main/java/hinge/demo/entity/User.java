41765f4c280cb5463b40b3da13b743e30972f489
Humming-Moon-afk <milian.loechner@gmail.com>
2026-08-22 11:00:56 +0200

    refactor(user): simplify field names and switch id to uuid

--- java_backend/src/main/java/hinge/demo/entity/User.java
+++ java_backend/src/main/java/hinge/demo/entity/User.java
@@ -4,41 +4,44 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "app_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private String userId;
-    private String userName;
-    private String userPhoneNumber;
-    private String userAdress;
-    public User(String userName, String userPhoneNumber, String userAdress) {
-        this.userName = userName;
-        this.userPhoneNumber = userPhoneNumber;
-        this.userAdress = userAdress;
+    private java.util.UUID id;
+    private String name;
+    private String phoneNumber;
+    private String address;
+    public User(String name, String phoneNumber, String address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
     }
-    public User() {}
-    public String getId() {
-        return userId;
+    public User() {
+
+    }
+    public java.util.UUID getId() {
+        return id;
     }
     public String getName() {
-        return userName;
+        return name;
     }
-    public void setName(String userName) {
-        this.userName = userName;
+    public void setName(String name) {
+        this.name = name;
     }
     public String getPhoneNumber() {
-        return userPhoneNumber;
+        return phoneNumber;
     }
-    public void setPhoneNumber(String userPhoneNumber) {
-        this.userPhoneNumber = userPhoneNumber;
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
-    public String getAdress() {
-        return userAdress;
+    public String getAddress() {
+        return address;
     }
-    public void setAdress(String userAdress) {
-        this.userAdress = userAdress;
+    public void setAddress(String address) {
+        this.address = address;
     }
-    
 }
