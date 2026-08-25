4e30ca08217ccb719e71154f8ac350764e2c7aa5
Humming-Moon-afk <milian.loechner@gmail.com>
2026-08-25 10:33:24 +0200

    feat(preference(: add gender preference

--- src/app/preferences/dating.tsx
+++ src/app/preferences/dating.tsx
@@ -1,10 +1,16 @@
 import { useLocalSearchParams } from "expo-router"
-import { Text, View } from "react-native"
+import { useState } from "react"
+import { Text, TouchableOpacity, View } from "react-native"
 export default function Dating() {
     const {userId} = useLocalSearchParams<{userId: string}>()
+    const [preference, setPreference] = useState('')
     return(
-        <View>
-            <Text>Dating Präferenzen</Text>
+        <View style = {{alignItems: 'center', paddingTop: 300, gap: 20}}>
+            <Text style = {{fontSize: 18, fontWeight:'bold'}}>Dating Präferenzen</Text>
+            <Text style = {{fontSize:15, fontWeight:'light'}}>Welches Geschlecht bevorzugst du?</Text>
+            <TouchableOpacity>
+                
+            </TouchableOpacity>
         </View>
     )
 }
