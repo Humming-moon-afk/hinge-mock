f16603eb030cbcdb1b5d9a6da42123553cde47ce
Humming-Moon-afk <milian.loechner@gmail.com>
2026-08-25 11:11:44 +0200

    feat(preference): add gender selection

--- src/app/preferences/dating.tsx
+++ src/app/preferences/dating.tsx
@@ -1,23 +1,36 @@
 import { useLocalSearchParams, useRouter } from "expo-router"
 import { useState } from "react"
-import { Text, View } from "react-native"
+import { Text, TouchableOpacity, View } from "react-native"
 
-
-
-
-export default function dating(){
-    const {userId} = useLocalSearchParams<{userId: string}>()
+export default function Dating() {
+    const { userId } = useLocalSearchParams<{ userId: string }>()
     const preferences = ['Männlich', 'Weiblich', 'Beide']
     const [preference, setPreference] = useState('')
     const router = useRouter()
-    return(
-        <View style = {{alignItems:'center', gap:20, paddingTop: 200}}>
-            <Text style = {{fontWeight:'bold', fontSize:18}}>
+
+    return (
+        <View style={{ alignItems: 'center', gap: 20, paddingTop: 200 }}>
+            <Text style={{ fontWeight: 'bold', fontSize: 18 }}>
                 Dating Präferenzen
             </Text>
-            <Text style = {{fontWeight:'condensed', fontSize:15}}>
+            <Text style={{ fontSize: 15 }}>
                 Wen möchtest du daten?
             </Text>
+
+            <View style={{ width: '80%', gap: 10 }}>
+                {preferences.map((item) => {
+                    const isSelected = preference === item
+
+                    return (
+                        <TouchableOpacity 
+                            key={item} 
+                            onPress={() => setPreference(item)}
+                        >
+                            <Text>{item}</Text>
+                        </TouchableOpacity>
+                    )
+                })}
+            </View>
         </View>
     )
 }
