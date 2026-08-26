21103fe3f0245ff8f86821bdab8d295caf52eef8
Humming-Moon-afk <milian.loechner@gmail.com>
2026-08-26 16:52:21 +0200

    feat(relationship): initialize screen state and preference options

--- src/app/preferences/relationship.tsx
+++ src/app/preferences/relationship.tsx
@@ -1,6 +1,14 @@
+import { useLocalSearchParams, useRouter } from "expo-router"
+import { useState } from "react"
 
 
 
 export default function Relationship() {
+    const router = useRouter()
+    const [relationship, setRelationship] = useState('')
+    const { userId, preference} = useLocalSearchParams<{userId: string, preference: string}>()
+    const datingPreferences = ['Langfristig', 'Kurzfristig', 'Langfristig, offen für kurzfristig', 'Kurzfristig, offen für langfristig']
+    return(
 
+    )
 }
